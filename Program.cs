556721565274584cbb9014d0b49b42ce32808f20
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:57:05 +0200

    ok

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T05Ejercicio15
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = { 9, 5, 4, 3, 3, 2, 1 };
+            //Utilizo el método Reverse de System.Linq
+            //para invertir los valores de un array
+            int[] arrayInverso = array.Reverse().ToArray();
+
+            Console.WriteLine("Los elementos del array original por orden son: ");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Los elementos del array original invertidos son: ");
+            foreach (int i in arrayInverso)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
